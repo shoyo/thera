413118f25725c42f61042c43c983d2b2059a2000
shoyo-inokuchi <shoyoinokuchi@gmail.com>
2019-02-16 02:34:36 +0900

    add result.html, display raw json responses

--- main_app/views.py
+++ main_app/views.py
@@ -13,19 +13,33 @@ def index(request):
     return render(request, 'main_app/index.html')
 
 
+# def result(request):
+#     text = request.GET['input']
+#     ret = {'data': analyze_sentiment(text)}
+#     return render(request, 'main_app/result.html', ret)
+
 def result(request):
-    text = request.GET['input']
-    ret = {'data': analyze_sentiment(text)}
-    return render(request, 'main_app/result.html', ret)
+    ret = {} # all query types to JSON responses
+    query2url = {
+        # query type to base URL
+        'sentiment': 'https://aylien-text.p.rapidapi.com/sentiment?text=',
+        'emotion': 'https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/?text=',
+    }
+    text = request.GET['input'] # get input text from form
+    for query_type in query2url.keys():
+        json_response = get_api_response(query2url[query_type], text)
+        ret[query_type] = json_response
+    return render(request, 'main_app/result.html', {'all': ret})
 
 
 # Helper functions
 
-def analyze_sentiment(text):
-    """Return dictionary containing sentiment data of text."""
-    base_url = 'https://aylien-text.p.rapidapi.com/sentiment?text='
+# def get_emotion()
+
+def get_api_response(api_base_url, text):
+    """Return JSON response from API base url given the input text."""
     query = url_encode(text)
-    response = requests.get(base_url + query,
+    response = requests.get(api_base_url + query,
                             headers={"X-RapidAPI-Key": os.environ['RAPID_API_KEY']})
     return response.json()
 
