178b373289b03f868b74ceedd016938f594bb2d3
tigga36 <onotaiga36@gmail.com>
2019-02-15 23:24:09 +0900

    setup basic top page template

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'main_app/index.html')
--- main_project/settings.py
+++ main_project/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'main_app',
 ]
 
 MIDDLEWARE = [
