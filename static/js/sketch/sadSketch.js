4723ecc1adb58c90d99b0eaf348dd9f2ba3c414f
Rick <koberick@me.com>
2019-02-17 04:21:40 +0900

    New Happy animation. New Logo colors.

--- static/js/sketch/sadSketch.js
+++ static/js/sketch/sadSketch.js
@@ -1,8 +1,9 @@
 function sadSet(){
   count = 5;
   ripples = new Array(count);
+  var clr = color(255,255,255,77);
   for (var i=0;i<ripples.length;i++){
-    ripples[i] = new Ripple(random(0,width), random(0,height), 250, 1, 0, 2);
+    ripples[i] = new Ripple(random(0,width), random(0,height), 250, 2, 2, 1, 0, 2, 3, clr);
     console.log(ripples[i]);
   }
 }
