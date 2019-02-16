32ef687a18909330202e2cc831dc2f74de846e6c
soda0398 <skaieda@yahoo.com>
2019-02-16 21:03:42 +0900

    test 2

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('redirect/', views.redirect_view),
     path('<str:username>/journal', views.journal, name='journal'),
     path('signup/', views.signup, name='signup'),
-    path('get_ip',views.get_client_ip,name='client_id'),
+    path('get_ip/',views.get_client_ip,name='client_id'),
     # path('signin/', auth_views.login, name='login'),
     # path('signout/', auth_views.logout, name='logout'),
 ]
